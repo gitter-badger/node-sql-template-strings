5f2928ef8fcd3999d648f961da48490f3734ed3c
Felix Becker <felix.b@outlook.com>
2016-08-26 13:05:57 +0200

    Switch to TypeScript

--- index.d.ts
+++ index.d.ts
@@ -1,68 +1,59 @@
-
-declare class SQLStatement {
-
-  private strings: string[];
-
-  /**
-   * The SQL Statement for [node-postgres](https://www.npmjs.com/package/pg)
-   */
-  text: string;
-
-  /**
-   * The SQL Statement for [Sequelize](https://www.npmjs.com/package/sequelize)
-   */
-  query: string
-
-  /**
-   * The SQL Statement for [mysql](https://www.npmjs.com/package/mysql)
-   */
-  sql: string;
-
-  /**
-   * The values to be inserted for the placeholders
-   */
-  values: any[];
-
-  /**
-   * The name for postgres prepared statements, if set
-   */
-  name: string;
-
-  /**
-   * Appends a string or another statement
-   *
-   * ```ts
-   * query.append(SQL`AND genre = ${genre}`).append(' ORDER BY rating')
-   * query.text   // => 'SELECT author FROM books WHERE name = $1 AND author = $2 AND genre = $3 ORDER BY rating'
-   * query.sql    // => 'SELECT author FROM books WHERE name = ? AND author = ? AND genre = ? ORDER BY rating'
-   * query.values // => ['harry potter', 'J. K. Rowling', 'Fantasy'] ORDER BY rating`
-   *
-   * const query = SQL`SELECT * FROM books`
-   * if (params.name) {
-   *   query.append(SQL` WHERE name = ${params.name}`)
-   * }
-   * query.append(SQL` LIMIT 10 OFFSET ${params.offset || 0}`)
-   * ```
-   */
-  append(statement: SQLStatement|string|number): this;
-
-  /**
-   * Sets the name property of this statement for prepared statements in postgres
-   *
-   * ```ts
-   * pg.query(SQL`SELECT author FROM books WHERE name = ${book}`.setName('my_query'))
-   * ```
-   */
-  setName(name: string): this;
-
-  /**
-   * Use a prepared statement with Sequelize.
-   * Makes `query` return a query with `$n` syntax instead of `?`  and switches the `values` key name to `bind`
-   * If omitted, `value` defaults to `true`.
-   */
-  useBind(value?: boolean): this;
+export declare class SQLStatement {
+    /**
+     * The SQL Statement for [mysql](https://www.npmjs.com/package/mysql)
+     */
+    sql: string;
+    /**
+     * The SQL Statement for [Sequelize](https://www.npmjs.com/package/sequelize)
+     */
+    query: string;
+    /**
+     * The SQL Statement for [node-postgres](https://www.npmjs.com/package/pg)
+     */
+    text: string;
+    /**
+     * The values to be inserted for the placeholders
+     */
+    values: string[];
+    /**
+     * The name for postgres prepared statements, if set
+     */
+    name: string;
+    /**
+     * Replacements for [Sequelize](https://www.npmjs.com/package/sequelize) in case bound queries are used
+     */
+    bind: string[];
+    private strings;
+    constructor(strings: string[], values: any[]);
+    /**
+     * Appends a string or another statement
+     *
+     * ```ts
+     * query.append(SQL`AND genre = ${genre}`).append(' ORDER BY rating')
+     * query.text   // => 'SELECT author FROM books WHERE name = $1 AND author = $2 AND genre = $3 ORDER BY rating'
+     * query.sql    // => 'SELECT author FROM books WHERE name = ? AND author = ? AND genre = ? ORDER BY rating'
+     * query.values // => ['harry potter', 'J. K. Rowling', 'Fantasy'] ORDER BY rating`
+     *
+     * const query = SQL`SELECT * FROM books`
+     * if (params.name) {
+     *   query.append(SQL` WHERE name = ${params.name}`)
+     * }
+     * query.append(SQL` LIMIT 10 OFFSET ${params.offset || 0}`)
+     * ```
+     */
+    append(statement: SQLStatement | string | number): this;
+    /**
+     * Use a prepared statement with Sequelize.
+     * Makes `query` return a query with `$n` syntax instead of `?`  and switches the `values` key name to `bind`
+     * If omitted, `value` defaults to `true`.
+     */
+    useBind(value?: boolean): this;
+    /**
+     * @param {string} name
+     * @returns {this}
+     */
+    setName(name: string): this;
 }
-
 /**
  * The template string tag
  *
@@ -72,5 +63,5 @@ declare class SQLStatement {
  * pg.query(SQL`SELECT author FROM books WHERE name = ${book} AND author = ${author}`)
  * ```
  */
-export function SQL(strings: string[], ...values: any[]): SQLStatement;
+export declare function SQL(strings: string[], ...values: any[]): SQLStatement;
 export default SQL;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,49 @@
-"use strict";
-var SQLStatement = (function () {
-    function SQLStatement(strings, values) {
+
+module.exports = exports = SQL;
+
+export class SQLStatement {
+
+    /**
+     * The SQL Statement for [mysql](https://www.npmjs.com/package/mysql)
+     */
+    public sql: string;
+
+    /**
+     * The SQL Statement for [Sequelize](https://www.npmjs.com/package/sequelize)
+     */
+    public get query() {
+        return this.bind ? this.text : this.sql;
+    }
+
+    /**
+     * The SQL Statement for [node-postgres](https://www.npmjs.com/package/pg)
+     */
+    public get text() {
+        return this.strings.reduce((prev, curr, i) => prev + '$' + i + curr);
+    }
+
+    /**
+     * The values to be inserted for the placeholders
+     */
+    public values: string[];
+
+    /**
+     * The name for postgres prepared statements, if set
+     */
+    public name: string;
+
+    /**
+     * Replacements for [Sequelize](https://www.npmjs.com/package/sequelize) in case bound queries are used
+     */
+    public bind: string[];
+
+    private strings: string[];
+
+    constructor(strings: string[], values: any[]) {
         this.strings = strings;
         this.values = values;
     }
-    Object.defineProperty(SQLStatement.prototype, "query", {
-        /**
-         * The SQL Statement for [Sequelize](https://www.npmjs.com/package/sequelize)
-         */
-        get: function () {
-            return this.bind ? this.text : this.sql;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(SQLStatement.prototype, "text", {
-        /**
-         * The SQL Statement for [node-postgres](https://www.npmjs.com/package/pg)
-         */
-        get: function () {
-            return this.strings.reduce(function (prev, curr, i) { return prev + '$' + i + curr; });
-        },
-        enumerable: true,
-        configurable: true
-    });
+
     /**
      * Appends a string or another statement
      *
@@ -40,51 +60,50 @@ var SQLStatement = (function () {
      * query.append(SQL` LIMIT 10 OFFSET ${params.offset || 0}`)
      * ```
      */
-    SQLStatement.prototype.append = function (statement) {
+    append(statement: SQLStatement | string | number): this {
         if (statement instanceof SQLStatement) {
             this.strings[this.strings.length - 1] += statement.strings[0];
             this.strings.push.apply(this.strings, statement.strings.slice(1));
             (this.values || this.bind).push.apply(this.values, statement.values);
-        }
-        else {
+        } else {
             this.strings[this.strings.length - 1] += statement;
         }
         return this;
-    };
+    }
+
     /**
      * Use a prepared statement with Sequelize.
      * Makes `query` return a query with `$n` syntax instead of `?`  and switches the `values` key name to `bind`
      * If omitted, `value` defaults to `true`.
      */
-    SQLStatement.prototype.useBind = function (value) {
-        if (value === void 0) { value = true; }
+    useBind(value: boolean = true): this {
         if (value && !this.bind) {
             this.bind = this.values;
             delete this.values;
-        }
-        else if (!value && this.bind) {
+        } else if (!value && this.bind) {
             this.values = this.bind;
             delete this.bind;
         }
         return this;
-    };
+    }
+
     /**
      * @param {string} name
      * @returns {this}
      */
-    SQLStatement.prototype.setName = function (name) {
+    setName(name: string): this {
         this.name = name;
         return this;
-    };
-    return SQLStatement;
-}());
-exports.SQLStatement = SQLStatement;
+    }
+}
+
 Object.defineProperty(SQLStatement.prototype, 'sql', {
     enumerable: true,
-    get: function () {
+    get() {
         return this.strings.join('?');
     }
 });
+
 /**
  * The template string tag
  *
@@ -94,16 +113,9 @@ Object.defineProperty(SQLStatement.prototype, 'sql', {
  * pg.query(SQL`SELECT author FROM books WHERE name = ${book} AND author = ${author}`)
  * ```
  */
-function SQL(strings) {
-    var values = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        values[_i - 1] = arguments[_i];
-    }
+export function SQL(strings: string[], ...values: any[]) {
     // the strings argument is a read-only array, which is why we need to clone it
     return new SQLStatement(strings.slice(0), values.slice(0));
 }
-exports.SQL = SQL;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = SQL;
-module.exports = SQL;
-//# sourceMappingURL=index.js.map
+
+export default SQL;
